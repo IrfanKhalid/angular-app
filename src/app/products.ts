9dc353185e7e7faf41663963ce0aaf023c6ececc
IrfanKhalid <rfnkhalid@gmail.com>
2019-07-29 20:46:06 +0500

    5 files modified, 3 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,11 +2,12 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    QualityRating:'A'
   },
   {
     name: 'Phone Mini',
-    price: 699,
+    price: 799,
     description: 'A great phone with one of the best cameras'
   },
   {
